c1afe28319ce815a8fadc8d63633ba598234688b
zpawn <ilia.makarov@me.com>
2017-02-10 17:06:25 +0200

    Hello, World!

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -122,4 +122,8 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionHello () {
+        return $this->render('hello');
+    }
 }
---
+++ views/site/hello.php
@@ -0,0 +1,6 @@
+<?php
+$this->title = 'hello';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<h1>Hello, World!</h1>
