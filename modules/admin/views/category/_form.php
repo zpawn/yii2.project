8cfa18b42131a50f240f9246aad642993e38dfdb
zpawn <ilia.makarov@me.com>
2017-03-10 15:49:06 +0200

    Admin: Show parent category in update page

--- modules/admin/views/category/_form.php
+++ modules/admin/views/category/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\modules\admin\models\Category;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Category */
@@ -12,7 +14,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'parent_id')->textInput() ?>
+    <?php //echo $form->field($model, 'parent_id')->textInput() ?>
+    <?= $form->field($model, 'parent_id')->dropDownList(ArrayHelper::map(Category::find()->all(), 'id','name')); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
